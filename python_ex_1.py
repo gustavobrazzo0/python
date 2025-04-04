00db9b8c1a6a7740d3692231a50efdb655fc18fa
gustavobrazzo0 <gustavobrazzo999#gmail.com>
2025-04-03 22:52:28 -0300

    limpando a master

--- python_ex_1.py
+++
@@ -1,6 +0,0 @@
-'''
-1. Faça um Programa que mostre a mensagem "Hello World! " na tela.
-
-'''
-
-print ("Hello World ! You are mine !")
--- python_ex_2.py
+++
@@ -1,9 +0,0 @@
-'''
-
-Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
-
-'''
-
-numero = int(input("Informe um número "))
-
-print("O número informado foi: {}".format(numero))
